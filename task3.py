31e4f2afb8632a70a7c62c58209f9ce3163e4cb6
khoahuynh0110 <kay.huynh0110@gmail.com>
2024-09-16 12:24:51 -0500

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+def read_spam():
+    category = 'spam'
+    directory = './enron1/spam'
+    return read_category(category, directory)
+
+def read_ham():
+    category = 'ham'
+    directory = './enron1/ham'
+    return read_category(category, directory)
+
+def read_category(category, directory):
+    emails = []
+    for filename in os.listdir(directory):
+        if not filename.endswith(".txt"):
+            continue
+        with open(os.path.join(directory, filename), 'r') as fp:
+            try:
+                content = fp.read()
+                emails.append({'name': filename, 'content': content, 'category': category})
+            except:
+                print(f'skipped {filename}')
+    return emails
+
+ham = read_ham()
+spam = read_spam()
+
+df = pd.DataFrame.from_records(ham)
+df = df.append(pd.DataFrame.from_records(spam))
